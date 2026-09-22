e274bb280e538add6a2011e991f7d5c89bf97a9a
Ντανιελ Βερααρτ <veraartdaniel@gmail.com>
2026-09-22 11:02:50 +0300

    Deleted web folder

--- web/config.js
+++
@@ -1,5 +0,0 @@
-// Supabase project connection details.
-// SUPABASE_KEY is a "publishable" key - it is safe to expose in frontend
-// code. It can only do what your Row Level Security policies allow.
-export const SUPABASE_URL = "https://gmpdylbjbcgeyqhdeors.supabase.co";
-export const SUPABASE_KEY = "sb_publishable_oDHVgRb_rTQH-XdDW-ZhdA_1GHVlVe5";
